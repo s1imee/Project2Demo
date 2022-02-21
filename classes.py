2f4a3c13d6efc9892cc50075099f0235997ab695
Artur <asintimur@gmail.com>
2022-02-21 16:42:12 +0300

    временно

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+from random import choice
 
 pygame.init()
 tile_width = tile_height = 56
@@ -53,13 +54,14 @@ class Bunker(pygame.sprite.Sprite):
 
 
 class Hero(pygame.sprite.Sprite):
-    image1 = pygame.image.load("data/tank_sand.png")
+    image1 = pygame.image.load("data/1111.png")
     original_image = pygame.transform.rotate(pygame.transform.scale(image1, (42, 46)), 90)
     image = pygame.transform.rotate(pygame.transform.scale(image1, (42, 46)), 90)
 
     def __init__(self, pos_x, pos_y, *group):
         super().__init__(*group)
         self.original_image = Hero.original_image
+        self.original_image = pygame.transform.rotate(self.original_image, 180)
         self.key_is_up = True
         self.rect = self.original_image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
@@ -114,7 +116,7 @@ class Hero(pygame.sprite.Sprite):
 
 class Bullet(pygame.sprite.Sprite):
     image1 = pygame.image.load("data/bulletRed2.png")
-    image = pygame.transform.scale(image1, (16, 24))
+    image = pygame.transform.scale(image1, (10, 14))
 
     def __init__(self, pos_x, pos_y, player, *group):
         super().__init__(*group)
@@ -128,7 +130,7 @@ class Bullet(pygame.sprite.Sprite):
         len = math.hypot(dx, dy)
         self.dx = dx / len
         self.dy = dy / len
-        self.speed = 10
+        self.speed = 20
         angle = math.degrees(math.atan2(-dy, dx)) - 90
         self.image = pygame.transform.rotate(self.image, angle)
 
@@ -156,43 +158,128 @@ class Camera:
 
 
 class Enemy(pygame.sprite.Sprite):
-    image1 = pygame.image.load("data/creature.jpg")
-    image = pygame.transform.scale(image1, (56, 56))
+    image1 = pygame.image.load("data/creature.png")
+    image = pygame.transform.scale(image1, (72, 84))
+    image_damage = pygame.image.load("data/creature_damge.png")
 
-    def __init__(self, *group):
+    def __init__(self, x, y, collidable_object, *group):
         super().__init__(*group)
+
+        self.collidable_object = []
+        for i in collidable_object:
+            if type(i) == Bunker:
+                self.bunker = i
+                self.collidable_object.append(i)
+            if type(i) == Tile:
+                if i.type == 'wall':
+                    self.collidable_object.append(i)
+
         self.image = Enemy.image
         self.rect = self.image.get_rect()
-        # self.rect.x = random.randint(0, 840)
-        # self.rect.y = random.randint(0, 840)
-        self.rect.x = 500
-        self.rect.y = 200
-        self.A = 448 - self.rect.x
-        self.B = 392 - self.rect.y
-        self.diag = (self.A ** 2 + self.B ** 2) ** (1 / 2)
-        self.dx = self.A / self.diag
-        self.dy = self.B / self.diag
-        print(self.dx)
-        print(self.dy)
+        self.rect.x, self.rect.y = x, y
+        self.hp = 100
+        self.speed = 1
+        self.up_or_down = choice(['up', 'down'])
+        self.lef_or_rig = choice(['left', 'rig'])
+        self.last_move_x = 0
+        self.last_move_y = 0
+        len_for_bunker_x = self.bunker.rect.x - self.rect.x
+        len_for_bunker_y = self.bunker.rect.y - self.rect.y
+
+        if abs(len_for_bunker_x) > abs(len_for_bunker_y):
+            if len_for_bunker_x > 0:
+                self.bypass_speed_x = 2
+            else:
+                self.bypass_speed_x = -2
+            self.bypass_speed_y = choice([2, -2])
+        else:
+            if len_for_bunker_y > 0:
+                self.bypass_speed_y = 2
+            else:
+                self.bypass_speed_y = -2
+            self.bypass_speed_x = choice([2, -2])
+
 
     def update(self):
-        if self.rect.x < 448 and self.rect.y < 392:
-            self.rect.x += self.dx
-            self.rect.y += self.dy
-        elif self.rect.x > 448 and self.rect.y < 392:
-            self.rect.x -= self.dx
-            self.rect.y += self.dy
-        elif self.rect.x < 448 and self.rect.y > 392:
-            self.rect.x += self.dx
-            self.rect.y -= self.dy
-        elif self.rect.x > 448 and self.rect.y > 392:
-            self.rect.x += self.dx
-            self.rect.y += self.dy
-        elif self.rect.x == 448 and self.rect.y < 392:
-            self.rect.y += self.B
-        elif self.rect.x == 448 and self.rect.y > 392:
-            self.rect.y -= self.B
-        elif self.rect.x < 448 and self.rect.y == 392:
-            self.rect.x += self.A
-        elif self.rect.x > 448 and self.rect.y == 392:
-            self.rect.x -= self.A
+        x = self.bunker.rect.x
+        y = self.bunker.rect.y
+        self.last_move_x = 0
+        self.last_move_y = 0
+
+        if self.rect.x > x:
+            self.rect.x -= self.speed
+            self.last_move_x = -self.speed
+        elif self.rect.x < x:
+            self.rect.x += self.speed
+            self.last_move_x = self.speed
+
+        check_collide = self.collide(self.collidable_object, 'x')
+        if check_collide[0]:
+            self.rect.x -= self.last_move_x
+            if check_collide[1]:  # подошли к бункеру или нет
+                self.attack_the_bunker()
+            else:
+                self.rect.y += self.bypass_speed_x
+
+        if self.rect.y > y:
+            self.rect.y -= self.speed
+            self.last_move_y = -self.speed
+        elif self.rect.y < y:
+            self.rect.y += self.speed
+            self.last_move_y = self.speed
+
+        check_collide = self.collide(self.collidable_object, 'y')
+        if check_collide[0]:
+            self.rect.y -= self.last_move_y
+            if check_collide[1]:  # подошли к бункеру или нет
+                self.attack_the_bunker()
+            else:
+                self.rect.x += self.bypass_speed_x
+
+
+    def took_damage(self, damage):
+        self.hp -= damage
+
+    def collide(self, collidable_object, x_or_y):
+        hits = pygame.sprite.spritecollide(self, collidable_object, False)
+        collide_x = False
+        collide_y = False
+        attack = False
+        if len(hits) != 0:
+            if x_or_y == 'x':
+                for block in hits:
+                    if block.rect.x + (block.rect.width - 10) < self.rect.x:  # left
+                        collide_x = True
+                    if block.rect.x > self.rect.x + (self.rect.width - 10):  # right
+                        collide_x = True
+                    if type(block) == Bunker:
+                        attack = True
+                if attack:
+                    self.attack_the_bunker()
+                return [collide_x, attack]
+            else:
+                for block in hits:
+                    if block.rect.y > self.rect.y + (self.rect.height - 10):  # up
+                        collide_y = True
+                    if block.rect.y + (block.rect.height - 10) < self.rect.y:  # down
+                        collide_y = True
+                    if type(block) == Bunker:
+                        attack = True
+                if attack:
+                    self.attack_the_bunker()
+                return [collide_y, attack]
+
+        return [False, attack]
+
+    def attack_the_bunker(self):
+        pass
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y, tile_images, *group):
+        super().__init__(*group)
+        self.type = tile_type
+        image1 = pygame.transform.scale(tile_images[tile_type], (56, 56))
+        self.image = image1
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
